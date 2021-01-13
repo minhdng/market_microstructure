3476a30d070ce48c9e2bd7c92e03236989aa66e9
hansenpei <50563403+hansenpei@users.noreply.github.com>
2021-01-12 23:04:18 -0500

    Now you can calculate R1
    See in main1.py for example. I am still working on aggregating trades by minutes

--- main1.py
+++ main1.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jan 10 22:16:51 2021
+Examples showing how to use top_order_book.py
 
 @author: Hansen
 """
@@ -14,9 +14,17 @@ import top_order_book as tob
 data = pd.read_csv(r'D:\MFM2021\Data\CMD_BBO\20201019\xcme-bbo-es-fut-20201019-r-00095_8.csv')
 mini_data = data.iloc[:100000]
 TV = tob.TradeVisualize(mini_data)
-#%% 
-trades_idx = TV.get_trades()
-prev_ab_df = TV.get_prev_ab_idx(trades_idx)
+my_data = TV.data
+#%%
+
+# Index where trade happens
+trade_idx = TV.get_trades()
+# Immediate prevailing bid and ask index
+prev_ab_df = TV.get_prev_ab_idx(trade_idx)
+
+#%% Get the immediate quotes, volume etc right before the trade for calculating R(1), page 213 (11.5)
+
+r1_info = TV.get_immediate_info(prev_ab_df)
 #%% testing plot
 plt.style.use('default')
 fig, ax = plt.subplots(dpi=300)
--- top_order_book.py
+++ top_order_book.py
@@ -90,7 +90,7 @@ class TradeVisualize:
             # Handle the continuous trade case, and move the pointer to the most recent non-trade pair.
             pointer = idx
             while True:
-                if pointer not in trades_idx:  # If not in there then it is a valid choise
+                if pointer not in trades_idx:  # If not in there then it is a valid choice
                     break
                 pointer -= 1  # Else, keep looking
 
@@ -103,6 +103,132 @@ class TradeVisualize:
         prev_ab_df = pd.DataFrame.from_dict(data=prev_ab_idx_dict, orient="index")
 
         return prev_ab_df
+    
+    def get_immediate_info(self, ab_df: pd.DataFrame, mid_type: str = 'simple') -> pd.DataFrame:
+        """
+        Get the immediate ask and bid info for a trade. 
+
+        Depends on if ad_df is for the prevailing quote or the immediate afterwards info.
+
+        Parameters
+        ----------
+        ab_df : pd.DataFrame
+            The ask and bid dataframe.
+        mid_type : str. Optional.
+            The type of mid price. Choices are 'simple' and 'weighted'. The default is 'simple'.
+
+        Returns
+        -------
+        immed_info_df : pd.DataFrame
+
+        """
+
+        trade_idx = ab_df.index
+        trade_idx_series = pd.Series(trade_idx)
+        dates = self.data.iloc[trade_idx]['Date'].reset_index(drop=True)
+        entry_dates = self.data.iloc[trade_idx]['EntryDate'].reset_index(drop=True)
+        times = self.data.iloc[trade_idx]['Time'].reset_index(drop=True)
+        trade_volumes = self.data.iloc[trade_idx]['Volume'].reset_index(drop=True)
+        ask_volumes = self.data.iloc[ab_df['ASK idx']]['Volume'].reset_index(drop=True)
+        bid_volumes = self.data.iloc[ab_df['BID idx']]['Volume'].reset_index(drop=True)
+        trade_prices = self.data.iloc[trade_idx]['Price'].reset_index(drop=True)
+        ask_prices =  self.data.iloc[ab_df['ASK idx']]['Price'].reset_index(drop=True)
+        bid_prices = self.data.iloc[ab_df['BID idx']]['Price'].reset_index(drop=True)
+        mid_prices = self._get_mid_prices(ask_volumes, bid_volumes, ask_prices, bid_prices, mid_type)
+        buy_or_sell = self._check_buy_or_sell(trade_prices, ask_prices, bid_prices)
+
+        # Assemble into a DataFrame
+        frame = {'TradeIdx': trade_idx_series,
+                 'Date': dates,
+                 'EntryDate': entry_dates,
+                 'Time': times,
+                 'TradeVolume': trade_volumes,
+                 'AskVolume': ask_volumes,
+                 'BidVolume': bid_volumes,
+                 'TradePrice': trade_prices,
+                 'AskPrice': ask_prices,
+                 'BidPrice': bid_prices,
+                 'MidPrice': mid_prices,
+                 'Buy/Sell': buy_or_sell}
+        immed_info_df = pd.DataFrame(frame)
+        
+        return immed_info_df
+        
+    def _get_mid_prices(self, ask_volumes: pd.Series, bid_volumes: pd.Series, ask_prices: pd.Series,
+                        bid_prices: pd.Series, mid_type: str) -> pd.Series:
+        """
+        Get mid prices for bid and ask.
+
+        Parameters
+        ----------
+        ask_volumes : pd.Series
+            Immediate prevailing ask volumes for a trade.
+        bid_volumes : pd.Series
+            Immediate prevailing bid volumes for a trade.
+        ask_prices : pd.Series
+            Immediate prevailing ask prices for a trade.
+        bid_prices : pd.Series
+            Immediate prevailing bid prices for a trade.
+        mid_type : str
+            Simple or weighted.
+
+        Raises
+        ------
+        ValueError
+            No such mid_type.
+
+        Returns
+        -------
+        mid_prices : pd.Series
+            Calculated mid prices.
+
+        """
+
+        if mid_type == 'simple':
+            mid_prices = (ask_prices + bid_prices) / 2
+
+        elif mid_type == 'weighted':
+            mid_prices = (ask_volumes * ask_prices + bid_volumes * bid_prices) / (ask_volumes + bid_volumes)
+
+        else:
+            raise ValueError('mid_type = "simple" or "weighted"')
+
+        return mid_prices
+            
+    def _check_buy_or_sell(self, trade_prices: pd.Series, ask_prices: pd.Series, bid_prices: pd.Series) -> pd.Series:
+        """
+        Find if a trade is a buy or sell order by matching with immediate prevailing price.
+        
+        1 means buy, -1 means sell, 0 means not clear.
+
+        Parameters
+        ----------
+        trade_prices : pd.Series
+            Price at which trade occured.
+        ask_prices : pd.DataFrame
+            The ask price immediately prevailing a trade.
+        bid_prices : pd.DataFrame
+            The bid price immediately prevailing a trade.
+
+        Returns
+        -------
+        buy_or_sell : pd.Series
+            The result recorded in 1 or -1 for buy or sell respectively.
+
+        """
+
+        buy_or_sell_data = np.zeros_like(trade_prices)
+        
+        for idx, trade_price in trade_prices.iteritems():
+            if trade_price > bid_prices.iloc[idx]:  # Greater than bid, so a buy.
+                buy_or_sell_data[idx] = 1
+            if trade_price < ask_prices.iloc[idx]:  # Smaller than ask, so a sell.
+                buy_or_sell_data[idx] = -1
+        
+        # Change into series.
+        buy_or_sell = pd.Series(buy_or_sell_data)
+        
+        return buy_or_sell
 
     def plot_abt(self, prev_ab_df: pd.DataFrame, ax: plt.axes = None) -> plt.axes:
         """
