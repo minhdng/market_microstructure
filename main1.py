1d5fa0e4bb4812f0f811dffb5811121ceba294b6
hansenpei <50563403+hansenpei@users.noreply.github.com>
2021-01-13 01:59:02 -0500

    Added lumping and plotting
    Lump continuous trades and plot the immediate bid/ask price before a trade. See main1 for example.

--- main1.py
+++ main1.py
@@ -12,8 +12,8 @@ import top_order_book as tob
 
 #%% Test, read data
 data = pd.read_csv(r'D:\MFM2021\Data\CMD_BBO\20201019\xcme-bbo-es-fut-20201019-r-00095_8.csv')
-mini_data = data.iloc[:100000]
-TV = tob.TradeVisualize(mini_data)
+mini_data = data.iloc[5000:10000]
+TV = tob.TradeVisualize(mini_data, lump_trades=True)
 my_data = TV.data
 #%%
 
@@ -24,10 +24,14 @@ prev_ab_df = TV.get_prev_ab_idx(trade_idx)
 
 #%% Get the immediate quotes, volume etc right before the trade for calculating R(1), page 213 (11.5)
 
-r1_info = TV.get_immediate_info(prev_ab_df)
-#%% testing plot
-plt.style.use('default')
+r1_info = TV.get_immediate_info(prev_ab_df, mid_type='weighted')
+summary = r1_info.describe()
+
+#%% Plot
+plt.style.use('seaborn')
 fig, ax = plt.subplots(dpi=300)
-ax = TV.plot_abt(prev_ab_df, ax)
+ax = TV.plot_by_trade(r1_info, ax)
 plt.tight_layout()
 plt.show()
+
+
