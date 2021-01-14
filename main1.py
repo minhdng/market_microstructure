7e09e0eb4538e52c61b4ff2c15c6e4bdfbccb613
hansenpei <50563403+hansenpei@users.noreply.github.com>
2021-01-13 20:51:46 -0500

    Aggregate by ticks added
    Now you can aggregate by ticks.
    Also you don't need to specify which mid value to calculate since it calculates both.

--- main1.py
+++ main1.py
@@ -16,32 +16,33 @@ mini_data = data
 TV = tob.TradeVisualize(mini_data, lump_trades=False)
 my_data = TV.data
 #%%
-
 # Index where trade happens
 trade_idx = TV.get_trades()
 
 # Immediate prevailing bid and ask index
 prev_ab_df = TV.get_prev_ab_idx(trade_idx, handle_conti_trade='bidask')
 
-#%% Get the immediate quotes, volume etc right before the trade for calculating R(1), page 213 (11.5)
+# Get the immediate quotes, volume etc right before the trade
+trades_info = TV.get_immediate_info(prev_ab_df)
 
-# weighted mid prices
-r1_info = TV.get_immediate_info(prev_ab_df, mid_type='weighted')
+summary = trades_info.describe()
 
-# simple mid prices
-r1_info_simple = TV.get_immediate_info(prev_ab_df, mid_type='simple')
-summary = r1_info.describe()
+#%% Get aggregated info by tick time
+aggre_info_tick_r0 = TV.get_aggre_info_tick(trades_info, duration=None)
 
-#%% Get aggregated impact
+# Look up 5 ticks, not including itself (r5)
+aggre_info_tick_r6 = TV.get_aggre_info_tick(trades_info, duration=5)
+
+#%% Get aggregated impact by actual time (in seconds)
 # For calculating R1, don't aggregate
-aggre_info_r1 = TV.get_aggre_info(r1_info, duration=None)
+aggre_info_r1 = TV.get_aggre_info(trades_info, duration=None)
 # Aggregating 5 seconds
-aggre_info_5 = TV.get_aggre_info(r1_info, duration=5)
+aggre_info_5 = TV.get_aggre_info(trades_info, duration=5)
 #%% Plot each trade for visualization
 # Note: Don't plot too much, generally around 200-400 data points works best for visualization
 plt.style.use('seaborn')
 fig, ax = plt.subplots(dpi=300)
-ax = TV.plot_by_trade(r1_info, ax, plot_mid_price = True)
+ax = TV.plot_by_trade(trades_info, ax, plot_mid_price = True)
 ax.grid()
 plt.tight_layout()
 plt.show()
